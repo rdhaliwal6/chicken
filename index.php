f5123bde7dda7bf9119d0282240b273ef5f4bba9
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-01-15 14:39:02 -0500

    Load F3

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
-<!--
-Rajpreet Dhaliwal
-01/15/2020
-/328/chicken/index
--->
 <?php
+//Rajpreet Dhaliwal
+//01/15/2020
+///328/chicken/index
+///
+
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Require autoload file
+require("vendor/autoload.php");
+
+//Instantiate F3
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function (){
+    echo '<h1>Hello, World!</h1>';
+}
+);
+
+//Run F3
+$f3->run();
